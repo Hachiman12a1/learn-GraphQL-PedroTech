6bc4b62eb09ce768ac21c4ac7e1e197408114ef7
Đào Văn Đạt <daovandat2k15@gmail.com>
2023-07-17 17:51:12 +0700

    GraphQL Resolvers

--- schema/resolvers.js
+++ schema/resolvers.js
@@ -1,9 +1,33 @@
-const { UserList } = require("../FakeData");
+const { UserList, MovieList } = require("../FakeData");
 
 const resolvers = {
   Query: {
-    users() {
+    // Users Resolvers
+    users: () => {
       return UserList;
+    },
+    user: (parent, args) => {
+      const id = args.id;
+      const user = UserList.find((user) => user.id === Number(id));
+      return user;
+    },
+    // Movies Resolvers
+    movies: () => {
+      return MovieList;
+    },
+    movie: (parent, args) => {
+      const name = args.name;
+      const movie = MovieList.find((movie) => movie.name === name);
+      return movie;
+    },
+  },
+  User: {
+    favoriteMovies: () => {
+      const favoriteMovies = MovieList.filter(
+        (movie) =>
+          movie.yearOfPublication >= 2000 && movie.yearOfPublication <= 2010
+      );
+      return favoriteMovies;
     },
   },
 };
--- schema/type-defs.js
+++ schema/type-defs.js
@@ -7,10 +7,29 @@ const typeDefs = gql`
     username: String!
     age: Int!
     nationality: String!
+    friends: [User]
+    favoriteMovies: [Movie]
+  }
+
+  type Movie {
+    id: ID!
+    name: String!
+    yearOfPublication: Int!
+    isInTheaters: Boolean!
   }
 
   type Query {
     users: [User!]!
+    user(id: ID!): User!
+    movies: [Movie!]!
+    movie(name: String!): Movie!
+  }
+  enum Nationality {
+    CANADA
+    BRAZIL
+    CHILE
+    INDIA
+    GERMANY
   }
 `;
 
